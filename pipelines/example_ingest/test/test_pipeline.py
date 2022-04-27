232d0127f82d7b518d723a877f906882ef8cb3ec
jmcvey3 <jmcvey3@users.noreply.github.com>
2022-04-26 20:13:08 -0700

    Initial commit

--- pipelines/example_ingest/test/test_pipeline.py
+++ pipelines/example_ingest/test/test_pipeline.py
@@ -8,10 +8,10 @@ def test_example_ingest_pipeline():
     config = PipelineConfig.from_yaml(config_path)
     pipeline = config.instantiate_pipeline()
 
-    test_file = "pipelines/example_ingest/test/data/input/data.csv"
-    expected_file = (
-        "pipelines/example_ingest/test/data/expected/abc.example.a1.20220424.000000.nc"
+    test_file = (
+        "pipelines/example_ingest/test/data/input/buoy.z06.00.20201201.000000.waves.csv"
     )
+    expected_file = "pipelines/example_ingest/test/data/expected/morro.buoy_z06-waves-20m.a1.20201201.000000.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
