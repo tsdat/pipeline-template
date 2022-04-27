c8b32ee5eec381793c32d4de681d891ee6240f87
jmcvey3 <jmcvey3@users.noreply.github.com>
2022-04-27 11:34:48 -0700

    Minor updates

---
+++ pipelines/example_pipeline/check_data.py
@@ -0,0 +1,8 @@
+# %%
+import xarray as xr
+
+ds = xr.open_dataset(
+    "../../storage/root/data/morro.buoy_z06-waves.a1/morro.buoy_z06-waves.a1.20201201.000000.nc"
+)
+
+# %%
--- pipelines/example_pipeline/test/test_pipeline.py
+++ pipelines/example_pipeline/test/test_pipeline.py
@@ -3,15 +3,13 @@ from pathlib import Path
 from tsdat import PipelineConfig, assert_close
 
 
-def test_example_ingest_pipeline():
-    config_path = Path("pipelines/example_ingest/config/pipeline.yaml")
+def test_example_pipeline_pipeline():
+    config_path = Path("pipelines/example_pipeline/config/pipeline.yaml")
     config = PipelineConfig.from_yaml(config_path)
     pipeline = config.instantiate_pipeline()
 
-    test_file = (
-        "pipelines/example_ingest/test/data/input/buoy.z06.00.20201201.000000.waves.csv"
-    )
-    expected_file = "pipelines/example_ingest/test/data/expected/morro.buoy_z06-waves.a1.20201201.000000.nc"
+    test_file = "pipelines/example_pipeline/test/data/input/buoy.z06.00.20201201.000000.waves.csv"
+    expected_file = "pipelines/example_pipeline/test/data/expected/morro.buoy_z06-waves.a1.20201201.000000.nc"
 
     dataset = pipeline.run([test_file])
     expected: xr.Dataset = xr.open_dataset(expected_file)  # type: ignore
